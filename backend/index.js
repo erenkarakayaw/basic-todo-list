3bbc30fd0d2e04ce568b16a343a99cd08ce288b2
erenkarakayaw <115037872+erenkarakayaw@users.noreply.github.com>
2025-08-08 16:30:15 +0300

    latest db integration

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,66 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const sqlite = require('sqlite')
+const sqlite = require('node:sqlite')
 const path = require('path')
 const port = 3000
-const db = sqlite.Database("./db.db");
+const db = new sqlite.DatabaseSync("db.db");
 
 app.use(cors({
     origin : 'http://localhost:5173'
 }))
 
 
-app.get('/', (req, res) => {
+app.get('/get', (req, res) => {
 
     let _datas = []
-
-    console.log(db.prepare("SELECT TODO, ISCOMPLETED FROM TODOS").all());
-
     
+    if (!db.isOpen) db.open()    
+    
+    db.prepare("SELECT TODO_ID, TODO, ISCOMPLETED FROM TODOS").all().forEach((item) => {
+        _datas.push({
+            todoID: item.TODO_ID,
+            todo: item.TODO,
+            isCompleted: Boolean(item.ISCOMPLETED)
+        })
+    })
+    
+    db.close()
+
+    res.status(200)
     res.json({datas: _datas});
+})
+
+app.get('/add', (req,res) => {
+    
+    const todo=req.query.todo    
+
+    if (!db.isOpen) db.open() 
+
+    db.exec(`INSERT INTO TODOS (TODO, ISCOMPLETED) 
+        VALUES (${todo}, 0)`)
+
+    db.close()
+
+    res.status(200)
+    res.send()
+})
+
+app.get('/update', (req,res) => {
+
+    const isCompleted = req.query.iscompleted
+    const todo = req.query.todoid
+    
+    if (!db.isOpen) db.open() 
+
+    db.exec(
+        `UPDATE TODOS SET ISCOMPLETED=${isCompleted} WHERE TODO_ID = ${todo}`
+    );       
+
+    db.close()
+
+    res.status(200)
+    res.send()
 })
 
 app.listen(port, () => console.log('started'));
--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,148 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { RiDeleteBin6Fill } from "react-icons/ri";
+import spinner from "./assets/tube-spinner.svg";
 
 function App() {
-
     const [Todos, setTodos] = useState([]);
-    
-    useEffect(() => {
-        fetch("http://localhost:3000/")
-                .then((res) => {
-                    if (res.ok) return res.json();
-                })
-                .then((data) => {
-                    setTodos(data.datas)
-                })
-                .catch((err) => console.log(err));
-    }, []);    
 
-    
+    useEffect(() => {
+        fetch("http://localhost:3000/get/")
+            .then((res) => {
+                if (res.ok) return res.json();
+            })
+            .then((data) => {
+                setTodos(data.datas);
+            })
+            .catch((err) => console.log(err))
+            .finally(() => setLoading(false));
+    }, []);
+
     const [todoInput, settodoInput] = useState("");
+    const [loading, setLoading] = useState(true);
 
     function updateTodo(index) {
         let temp = [...Todos];
         temp[index].isCompleted = !temp[index].isCompleted;
         setTodos(temp);
+
+        setLoading(true);
+        fetch(
+            `http://localhost:3000/update?todoid=${
+                temp[index].todoID
+            }&iscompleted=${temp[index].isCompleted ? 1 : 0}`
+        )
+            .then((res) => {
+                if (res.ok) return res.json();
+            })
+            .then((dat) => {})
+            .catch((err) => {})
+            .finally(() => {
+                setLoading(false);
+            });
     }
 
     function deleteTodo(index) {
         setTodos(Todos.filter((dat, ind) => ind != index));
     }
 
+    function addTodo() {
+        const text = todoInput.toString();
+        console.log(text)
+        if (todoInput.length > 0)
+        {
+            setTodos((prev) => [...prev, { todo: todoInput, isCompleted: false }]);
+            settodoInput("");
+        }
+        
+        setLoading(true);
+        fetch(
+            `http://localhost:3000/add?todo=${text}`
+        )
+            .then((res) => {
+                if (res.ok) return res.json();
+            })
+            .then((dat) => {})
+            .catch((err) => {})
+            .finally(() => {
+                setLoading(false);
+            });  
+                  
+    }
+
     return (
         <>
             <div className="h-screen flex items-center justify-center">
-                <ul className="text-2xl">
-                    <li className="flex mb-5 gap-5">
-                        <input
-                            onChange={(e) => settodoInput(e.target.value)}
-                            value={todoInput}
-                            type="text"
-                            className="border-2 border-black w-72 p-2 rounded-sm"
-                        />
-                        <button
-                            onClick={(e) => {
-                                setTodos((prev) => [
-                                    ...prev,
-                                    { todo: todoInput, isCompleted: false },
-                                ]);
-                                settodoInput("");
-                            }}
-                            className="rounded-sm duration-200 hover:bg-blue-600 active:bg-blue-700 cursor-pointer bg-blue-500 text-white w-24"
-                        >
-                            Add
-                        </button>
-                    </li>
-                    <li className="mb-2">
-                        <h1 className=" text-4xl font-bold">Todos:</h1>
-                        <div className="w-full h-1 bg-black"></div>
-                    </li>
-                    {Todos.map((dat, index) =>
-                        !dat.isCompleted ? (
-                            <li
-                                className="my-8 w-96 flex justify-between items-center"
-                                key={index}
+                {loading ? (
+                    <img src={spinner} className="w-24 h-24"></img>
+                ) : (
+                    <ul className="text-2xl">
+                        <li className="flex mb-5 gap-5">
+                            <input
+                                onChange={(e) => settodoInput(e.target.value)}
+                                value={todoInput}
+                                type="text"
+                                className="border-2 border-black w-72 p-2 rounded-sm"
+                            />
+                            <button
+                                onClick={(e) => addTodo()}
+                                className="rounded-sm duration-200 hover:bg-blue-600 active:bg-blue-700 cursor-pointer bg-blue-500 text-white w-24"
                             >
-                                <input
-                                    onChange={(e) => updateTodo(index)}
-                                    className="mr-5 w-5 h-5"
-                                    type="checkbox"
-                                />
-                                {dat.todo}
-                                <button
-                                    onClick={(e) => deleteTodo(index)}
-                                    className="ml-5 active:hover:bg-red-500/30 w-10 h-10 hover:bg-red-500/20 cursor-pointer rounded-full duration-200  flex justify-center items-center"
+                                Add
+                            </button>
+                        </li>
+                        <li className="mb-2">
+                            <h1 className=" text-4xl font-bold">Todos:</h1>
+                            <div className="w-full h-1 bg-black"></div>
+                        </li>
+                        {Todos.map((dat, index) =>
+                            !dat.isCompleted ? (
+                                <li
+                                    className="my-8 w-96 flex justify-between items-center"
+                                    key={index}
                                 >
-                                    <RiDeleteBin6Fill size={25} color="red" />
-                                </button>
-                            </li>
-                        ) : (
-                            <li
-                                className="my-8 w-96 justify-between line-through flex items-center"
-                                key={index}
-                            >
-                                <input
-                                    onChange={(e) => updateTodo(index)}
-                                    checked
-                                    className="mr-5 w-5 h-5"
-                                    type="checkbox"
-                                />
-                                {dat.todo}
-                                <button
-                                    onClick={(e) => deleteTodo(index)}
-                                    className="ml-5 active:hover:bg-red-500/30 w-10 h-10 hover:bg-red-500/20 cursor-pointer rounded-full duration-200  flex justify-center items-center"
+                                    <input
+                                        onChange={(e) => updateTodo(index)}
+                                        className="mr-5 w-5 h-5"
+                                        type="checkbox"
+                                    />
+                                    {dat.todo}
+                                    <button
+                                        onClick={(e) => deleteTodo(index)}
+                                        className="ml-5 active:hover:bg-red-500/30 w-10 h-10 hover:bg-red-500/20 cursor-pointer rounded-full duration-200  flex justify-center items-center"
+                                    >
+                                        <RiDeleteBin6Fill
+                                            size={25}
+                                            color="red"
+                                        />
+                                    </button>
+                                </li>
+                            ) : (
+                                <li
+                                    className="my-8 w-96 justify-between line-through flex items-center"
+                                    key={index}
                                 >
-                                    <RiDeleteBin6Fill size={25} color="red" />
-                                </button>
-                            </li>
-                        )
-                    )}
-                </ul>
+                                    <input
+                                        onChange={(e) => updateTodo(index)}
+                                        checked
+                                        className="mr-5 w-5 h-5"
+                                        type="checkbox"
+                                    />
+                                    {dat.todo}
+                                    <button
+                                        onClick={(e) => deleteTodo(index)}
+                                        className="ml-5 active:hover:bg-red-500/30 w-10 h-10 hover:bg-red-500/20 cursor-pointer rounded-full duration-200  flex justify-center items-center"
+                                    >
+                                        <RiDeleteBin6Fill
+                                            size={25}
+                                            color="red"
+                                        />
+                                    </button>
+                                </li>
+                            )
+                        )}
+                    </ul>
+                )}
             </div>
         </>
     );
